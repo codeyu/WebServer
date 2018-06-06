7498e0c0516813da6480ae2e52f5dc8d1b3bd944
codeyu <codeyu@gmail.com>
2018-06-06 22:50:53 +0800

    HttpListener

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # WebServer
+
+https://www.codeproject.com/Articles/859108/Writing-a-Web-Server-from-Scratch
---
+++ src/ForkHttpListener/HttpListenerRequestEventArgs.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace ForkHttpListener
+{
+    public sealed class HttpListenerRequestEventArgs : EventArgs
+    {
+        internal HttpListenerRequestEventArgs(HttpListenerRequest request, HttpListenerResponse response)
+        {
+            Request = request;
+            Response = response;
+        }
+
+        /// <summary>
+        /// Gets the Request.
+        /// </summary>
+        public HttpListenerRequest Request { get; private set; }
+
+        /// <summary>
+        /// Gets the Response.
+        /// </summary>
+        public HttpListenerResponse Response { get; private set; }
+    }
+}
